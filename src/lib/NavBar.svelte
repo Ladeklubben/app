322ea534b42c02751595efb1b62e2cde54ea0bc7
Pandananana <andreaswendelboe.dk@gmail.com>
2025-02-12 20:41:05 +0100

    MAde Navbar nice for android

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -14,6 +14,11 @@
 		<Menu style="font-size: 25px;" />
 		Settings
 	</div>
+
+	<div class="nav-element" on:click={() => goto("/settings")}>
+		<Menu style="font-size: 25px;" />
+		Settings
+	</div>
 </div>
 
 <style>
@@ -21,20 +26,21 @@
 		display: flex;
 		justify-content: space-around;
 		align-items: center;
-		padding: 10px 0;
-		background-color: var(--lk-blue-800);
+		padding: 10px 0 10px 0;
+		background-color: var(--lk-blue-950);
 		color: white;
-		min-height: 80px;
 		position: fixed;
 		bottom: 0;
 		width: 100%;
 		z-index: 1000;
+		border-top: 1px solid var(--lk-blue-800);
 	}
 
 	.nav-element {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		gap: 5px;
+		gap: 2px;
+		font-size: 10px;
 	}
 </style>
