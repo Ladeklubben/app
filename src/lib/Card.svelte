dfa7c77194b5a6154b4e872445244a7149ff85e4
Pandananana <andreaswendelboe.dk@gmail.com>
2025-02-17 21:02:40 +0100

    Card component added

---
+++ src/lib/Card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let title: string;
+
+</script>
+
+<div class="container">
+    <h1>{title}</h1>
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        padding: 20px;
+        border-radius: 1.5rem;
+        border: 1px solid var(--lk-blue-800);
+    }
+
+    h1 {
+        color: var(--lk-blue-100);
+        font-size: 1.5rem;
+        margin: 0;
+    }
+</style>
--- src/lib/PhaseOverview.svelte
+++ src/lib/PhaseOverview.svelte
@@ -1,6 +1,7 @@
 <!-- PhaseOverview.svelte -->
 <script>
     import PhaseBar from "$lib/PhaseBar.svelte";
+    import Card from "$lib/Card.svelte";
 
     const phases = [
         { label: "L1", percentage: 85 },
@@ -9,27 +10,11 @@
     ];
 </script>
 
-<div class="container">
-    <h1>Fase oversigt</h1>
+<Card title="Fase Oversigt">
     {#each phases as phase}
         <PhaseBar label={phase.label} percentage={phase.percentage} />
     {/each}
-</div>
+</Card>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
-        padding: 20px;
-        border-radius: 1.5rem;
-        border: 1px solid var(--lk-blue-800);
-        max-width: 600px;
-    }
-
-    h1 {
-        color: var(--lk-blue-100);
-        font-size: 1.5rem;
-        margin: 0;
-    }
 </style>
