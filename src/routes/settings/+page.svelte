edf8f93e99973fcf06dd3b0d82926f305e4a0f42
Pandananana <andreaswendelboe.dk@gmail.com>
2025-02-17 19:07:39 +0100

    User email shown on settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import { logout } from "$lib/stores/auth";
+    import { currentUser } from "$lib/stores/auth";
 </script>
 
 <h1>Settings</h1>
+{$currentUser?.email}
 <button on:click={logout}>Logout</button>
 
 
