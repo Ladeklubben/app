f0218178a9291fe9800e0b3cc4a92c37c85539a1
Pandananana <andreaswendelboe.dk@gmail.com>
2025-02-21 09:03:26 +0100

    Price chart

--- src/lib/PriceChart.svelte
+++ src/lib/PriceChart.svelte
@@ -87,6 +87,7 @@
                         return date.toLocaleTimeString("default", {
                             hour: "2-digit",
                             minute: "2-digit",
+                            hour12: false,
                         });
                     }),
                     datasets: [
