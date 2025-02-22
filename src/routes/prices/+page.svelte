0b3c7e4073f41f9aeb54eb110858ef822c3368f5
Pandananana <andreaswendelboe.dk@gmail.com>
2025-02-22 15:11:42 +0100

    SingleStatCard added to price page

--- src/routes/prices/+page.svelte
+++ src/routes/prices/+page.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { isLoading, error, fetchPrices } from "$lib/services/prices";
+    import { isLoading, error, fetchPrices, prices } from "$lib/services/prices";
+    import type { PriceData } from "$lib/types/prices";
     import Card from "$lib/components/ui/Card.svelte";
     import PriceChart from "$lib/components/features/prices/PriceChart.svelte";
+    import SingleStatCard from "$lib/components/ui/SingleStatCard.svelte";
 
-    onMount(() => {
-        fetchPrices();
-    });
+    function getCurrentPrice(prices: PriceData[]) {
+        const earliestPrice = prices[0];
+        const date = new Date(earliestPrice.Timestamp * 1000);
+        const hour = date.getHours().toString().padStart(2, "0");
+        const minutes = date.getMinutes().toString().padStart(2, "0");
+        const time = `${hour}:${minutes}`;
+        const price = (earliestPrice.Costprice_VAT / 100).toFixed(2);
+    
+        return { time, price };
+    }
+    
+    let currentPrice = { time: "00:00", price: "N/A" };
+    
+    onMount(async () => {
+        await fetchPrices();
+        if ($prices.length > 0) {
+            currentPrice = getCurrentPrice($prices);
+        }
+    })
 </script>
 
 <div class="wrapper">
-    <Card title="Prices">
+    <SingleStatCard description="Live Price - {currentPrice.time}" stat={currentPrice.price} unit="kr." />
+
+    <Card title="Price Chart">
         {#if $isLoading}
             <p>Loading...</p>
         {:else if $error}
