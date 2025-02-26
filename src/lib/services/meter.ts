20040c1be503727c6cb0fa067283d65ba4f50c3c
Pandananana <andreaswendelboe.dk@gmail.com>
2025-02-26 20:47:33 +0100

    Added last fetch time to meter card

--- src/lib/services/meter.ts
+++ src/lib/services/meter.ts
@@ -3,11 +3,19 @@ import { get } from '$lib/services/api';
 
 const formatter = new Intl.NumberFormat('en-DK');
 
+export const lastFetch = writable<string>('N/A');
 export const powerImport = writable<string>('N/A');
 
 export async function fetchpowerImport() {
     try {
         const response = await get('/installation/mainmeter_tmp', true);
+        const lastFetchDate = new Date();
+        const lastFetchTime = lastFetchDate.toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+        });
+        lastFetch.set(lastFetchTime);
         const totalPowerImport = Math.round(response.total_power_import * 1000);
         powerImport.set(formatter.format(totalPowerImport));
     } catch (error) {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import PhaseOverview from "$lib/components/features/meter/PhaseOverview.svelte";
     import SingleStatCard from "$lib/components/ui/SingleStatCard.svelte";
     import { onMount } from "svelte";
-    import { fetchpowerImport, powerImport } from "$lib/services/meter";
+    import { fetchpowerImport, powerImport, lastFetch } from "$lib/services/meter";
 
     onMount(() => {
         fetchpowerImport();
@@ -10,7 +10,7 @@
 </script>
 
 <div class="wrapper">
-    <SingleStatCard description="Live Usage" stat={$powerImport} unit="W" />
+    <SingleStatCard description="Live Usage - {$lastFetch}" stat={$powerImport} unit="W" />
     <PhaseOverview />
     
 </div>
