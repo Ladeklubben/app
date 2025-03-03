97123f2a2f05592ab41c1ed48f70e14e2b535ce2
Pandananana <andreaswendelboe.dk@gmail.com>
2025-03-03 21:17:50 +0100

    Added checkbox functionality to inputfield

--- src/lib/components/ui/InputField.svelte
+++ src/lib/components/ui/InputField.svelte
@@ -2,18 +2,31 @@
     export let id: string;
     export let type: string = "text";
     export let label: string;
-    export let value: string;
+    export let value: string | boolean;
     export let error: string;
+
+    // Type guard to ensure value is boolean for checkboxes
+    let isChecked: boolean | null = null;
+    if (type === "checkbox" && typeof value === "boolean") {
+        isChecked = value;
+    }
 </script>
 
-<label for={id}>{label}</label>
-<input
-    {type}
-    {id}
-    name={id}
-    bind:value
-    class:error-border={error}
-/>
+{#if type === "checkbox"}
+    <div class="check-wrapper">
+        <label for={id}>{label}</label>
+        <input
+            type="checkbox"
+            {id}
+            name={id}
+            bind:checked={isChecked}
+            class:error-border={error}
+        />
+    </div>
+{:else}
+    <label for={id}>{label}</label>
+    <input {type} {id} name={id} bind:value class:error-border={error} />
+{/if}
 {#if error}
     <span class="error-text">{error}</span>
 {/if}
@@ -53,5 +66,40 @@
     input[type="number"]::-webkit-inner-spin-button {
         -webkit-appearance: none;
         margin: 0;
+    }
+
+    .check-wrapper {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .check-wrapper input[type="checkbox"] {
+        width: 1.5rem;
+        height: 1.5rem;
+        appearance: none;
+        background-color: transparent;
+        border: 1px solid var(--lk-blue-500);
+        border-radius: 0.25rem;
+        display: grid;
+        place-content: center;
+    }
+
+    .check-wrapper input[type="checkbox"]::before {
+        content: "";
+        width: 1rem;
+        height: 1rem;
+        clip-path: polygon(14% 44%, 0 65%, 50% 100%, 100% 20%, 80% 0, 43% 62%);
+        background-color: var(--lk-blue-400);
+        transform: scale(0);
+        transition: transform 0.2s ease-in-out;
+    }
+
+    .check-wrapper input[type="checkbox"]:checked::before {
+        transform: scale(1);
+    }
+
+    .check-wrapper input[type="checkbox"]:focus {
+        outline: 2px solid var(--lk-blue-300);
     }
 </style>
