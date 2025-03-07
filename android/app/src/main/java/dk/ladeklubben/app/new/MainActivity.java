6c511d881ad75985f515dd3687633fbe5938fb6b
Pandananana <andreaswendelboe.dk@gmail.com>
2025-03-07 10:50:43 +0100

    Fixed android status and navigation bar color

--- android/app/src/main/java/dk/ladeklubben/app/new/MainActivity.java
+++ android/app/src/main/java/dk/ladeklubben/app/new/MainActivity.java
@@ -2,6 +2,7 @@ package dk.ladeklubben.apps;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.graphics.Color;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -9,6 +10,12 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set navigation bar color
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(Color.parseColor("#182b34")); 
+            getWindow().setStatusBarColor(Color.parseColor("#182b34"));
+        }
     }
 
     @Override
