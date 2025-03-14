071829e3808874f395571c696a8231d94b1a7813
Pandananana <andreaswendelboe.dk@gmail.com>
2025-03-14 14:13:21 +0100

    Progress Dots component added

---
+++ src/lib/components/ui/ProgressDots.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { totalDots = 5, activeDot = 0 } = $props();
+</script>
+
+<div class="flex w-full gap-2 justify-center">
+	{#each Array(totalDots).fill(0) as _, index}
+		<div
+			class="w-2 h-2 rounded-full"
+			class:bg-lk-blue-800={index !== activeDot}
+			class:bg-lk-blue-500={index === activeDot}
+		></div>
+	{/each}
+</div>
