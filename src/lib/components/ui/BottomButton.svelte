214eb83fff017ad4a7884c5e76d81f5ab8ebe9c9
Pandananana <andreaswendelboe.dk@gmail.com>
2025-03-14 14:21:52 +0100

    Refactored bottom button with progress bar into a UI component

---
+++ src/lib/components/ui/BottomButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Button from "$lib/components/ui/Button.svelte";
+    import ProgressDots from "$lib/components/ui/ProgressDots.svelte";
+
+    let { activeDot, totalDots, formID, buttonText="Submit" } = $$props;
+
+</script>
+
+<div class="fixed bottom-2 left-0 w-full flex flex-col p-4">
+    {#if activeDot !== undefined && totalDots !== undefined}
+        <ProgressDots {activeDot} {totalDots} />
+    {/if}
+    <Button type="submit" {formID}>{buttonText}</Button>
+</div>
--- src/routes/setup/site/+page.svelte
+++ src/routes/setup/site/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import Subpage from "$lib/components/ui/Subpage.svelte";
 	import InputField from "$lib/components/ui/InputField.svelte";
-	import Button from "$lib/components/ui/Button.svelte";
 	import Form from "$lib/components/ui/Form.svelte";
-	import ProgressDots from "$lib/components/ui/ProgressDots.svelte";
+	import BottomButton from "$lib/components/ui/BottomButton.svelte";
 	import { siteFormData, resetSiteFormData } from "$lib/services/site";
 	import { goto } from "$app/navigation";
 	import { onMount } from "svelte";
@@ -33,8 +32,5 @@
 		<InputField id="name" type="text" label="Site Name" bind:value={$siteFormData.name} error={errors.name} />
 	</Form>
 
-	<div class="fixed bottom-2 left-0 w-full flex flex-col p-4">
-		<ProgressDots activeDot={0} />
-		<Button type="submit" formID="siteForm">Next</Button>
-	</div>
+	<BottomButton formID="siteForm" buttonText="Next" />
 </Subpage>
