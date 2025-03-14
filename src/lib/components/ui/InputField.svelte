7e8c46be6751163ae11396a6155642b43633d782
Pandananana <andreaswendelboe.dk@gmail.com>
2025-03-14 16:27:40 +0100

    Created dropdwon input field

--- src/lib/components/ui/InputField.svelte
+++ src/lib/components/ui/InputField.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	let { id, type = "text", label, value = $bindable(), error } = $props();
+	let {
+		id,
+		type = "text",
+		label,
+		value = $bindable(),
+		error,
+		options = [],
+	} = $props();
 </script>
 
 {#if type === "checkbox"}
@@ -11,9 +18,24 @@
 			name={id}
 			bind:checked={value}
 			class="h-6 w-6 appearance-none rounded-md border border-lk-blue-500 bg-transparent checked:before:scale-100 checked:before:bg-lk-blue-400 focus:outline-none focus:ring-2 focus:ring-lk-blue-300
-				before:m-[4px] before:h-4 before:w-4 before:scale-0 before:clip-path-[polygon(14%_44%,0_65%,50%_100%,100%_20%,80%_0,43%_62%)] before:transition-transform before:duration-200 before:ease-in-out
-				{error ? 'border-lk-red-700' : ''}"
+                before:m-[4px] before:h-4 before:w-4 before:scale-0 before:clip-path-[polygon(14%_44%,0_65%,50%_100%,100%_20%,80%_0,43%_62%)] before:transition-transform before:duration-200 before:ease-in-out
+                {error ? 'border-lk-red-700' : ''}"
 		/>
+	</div>
+{:else if type === "dropdown"}
+	<div class="flex flex-col gap-3">
+		<label for={id} class="font-bold">{label}</label>
+		<select
+			{id}
+			name={id}
+			bind:value
+			class="rounded-2xl border border-lk-blue-500 bg-transparent p-3 text-lg text-lk-blue-50 focus:border-lk-blue-300 focus:outline-none
+                {error ? 'border-lk-red-700' : ''}"
+		>
+			{#each options as option}
+				<option value={option.value}>{option.label}</option>
+			{/each}
+		</select>
 	</div>
 {:else}
 	<div class="flex flex-col gap-3">
@@ -24,7 +46,7 @@
 			name={id}
 			bind:value
 			class="rounded-2xl border border-lk-blue-500 bg-transparent p-3 text-lg text-lk-blue-50 focus:border-lk-blue-300 focus:outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
-				{error ? 'border-lk-red-700' : ''}"
+                {error ? 'border-lk-red-700' : ''}"
 		/>
 	</div>
 {/if}
