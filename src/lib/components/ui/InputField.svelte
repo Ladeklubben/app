de479b1cb0598e911b438dbbb1c277c8797aef94
Pandananana <andreaswendelboe.dk@gmail.com>
2025-03-16 15:31:52 +0100

    Added disabled functionaity to inputfield

--- src/lib/components/ui/InputField.svelte
+++ src/lib/components/ui/InputField.svelte
@@ -5,6 +5,7 @@
 		label,
 		value = $bindable(),
 		error,
+		disabled = false,
 		options = [],
 	} = $props();
 </script>
@@ -16,6 +17,7 @@
 			type="checkbox"
 			{id}
 			name={id}
+			{disabled}
 			bind:checked={value}
 			class="h-6 w-6 appearance-none rounded-md border border-lk-blue-500 bg-transparent checked:before:scale-100 checked:before:bg-lk-blue-400 focus:outline-none focus:ring-2 focus:ring-lk-blue-300
                 before:m-[4px] before:h-4 before:w-4 before:scale-0 before:clip-path-[polygon(14%_44%,0_65%,50%_100%,100%_20%,80%_0,43%_62%)] before:transition-transform before:duration-200 before:ease-in-out
@@ -28,6 +30,7 @@
 		<select
 			{id}
 			name={id}
+			{disabled}
 			bind:value
 			class="rounded-2xl border border-lk-blue-500 bg-transparent p-3 text-lg text-lk-blue-50 focus:border-lk-blue-300 focus:outline-none
                 {error ? 'border-lk-red-600' : ''}"
@@ -44,6 +47,7 @@
 			{type}
 			{id}
 			name={id}
+			{disabled}
 			bind:value
 			class="rounded-2xl border border-lk-blue-500 bg-transparent p-3 text-lg text-lk-blue-50 focus:border-lk-blue-300 focus:outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
                 {error ? 'border-lk-red-600' : ''}"
