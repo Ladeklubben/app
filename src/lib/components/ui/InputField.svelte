1326be80b90dfd86bba1988deb0a8ab840c103af
Pandananana <andreaswendelboe.dk@gmail.com>
2025-03-24 08:46:40 +0100

    Added togle inputfield type

--- src/lib/components/ui/InputField.svelte
+++ src/lib/components/ui/InputField.svelte
@@ -24,6 +24,26 @@
                 {error ? 'border-lk-red-600' : ''}"
 		/>
 	</div>
+{:else if type === "toggle"}
+	<div class="flex items-center justify-between">
+		<label for={id} class="font-bold">{label}</label>
+		<label class="inline-flex items-center cursor-pointer">
+			<input
+				type="checkbox"
+				{id}
+				name={id}
+				{disabled}
+				bind:checked={value}
+				class="sr-only peer"
+			/>
+			<div
+				class="relative w-11 h-6 bg-lk-blue-500/20 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-lk-blue-300 rounded-full peer peer-checked:bg-lk-blue-400 
+                after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border after:border-lk-blue-500/50 after:rounded-full after:h-5 after:w-5 after:transition-all 
+                peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full
+                {disabled ? 'opacity-50 cursor-not-allowed' : ''}"
+			></div>
+		</label>
+	</div>
 {:else if type === "dropdown"}
 	<div class="flex flex-col gap-3">
 		<label for={id} class="font-bold">{label}</label>
