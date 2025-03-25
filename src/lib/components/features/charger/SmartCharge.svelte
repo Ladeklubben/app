780df4f232dc5d25e58c40fc55a28022e38bdc24
Pandananana <andreaswendelboe.dk@gmail.com>
2025-03-25 14:33:30 +0100

    Optional settings button added to card elements

--- src/lib/components/features/charger/SmartCharge.svelte
+++ src/lib/components/features/charger/SmartCharge.svelte
@@ -9,7 +9,7 @@
 	let chargeNow = $state(false);
 </script>
 
-<Card title="Smart Charge">
+<Card title="Smart Charge" dotsPath="/menu">
 	<p>Select when you need your car and we will charge it when electricity is cheap.</p>
 	<div class="flex flex-row justify-center gap-4 rounded-2xl border border-lk-blue-800" style="background: linear-gradient(to right, #59a6b7, #2e4d59);">
 		<TimePicker bind:selectedTime={timeH} defaultTime={12} maxValue={24} align="right"/>
--- src/lib/components/ui/Card.svelte
+++ src/lib/components/ui/Card.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	let { children, title } = $props();
+	import Dots from "~icons/mdi/dots-horizontal"
+	let { children, title, dotsPath = "" } = $props();
 </script>
 
 <div class="flex flex-col gap-5 p-5 rounded-2xl border border-lk-blue-800">
-	<h1 class="text-lk-blue-50 text-2xl m-0 font-black">{title}</h1>
+	<div class="flex flex-row justify-between items-center">
+		<h1 class="text-lk-blue-50 text-2xl m-0 font-black">{title}</h1>
+		{#if dotsPath}
+			<a href={dotsPath}>
+				<Dots class="text-lk-blue-50 text-2xl" />
+			</a>
+		{/if}
+	</div>
 	{@render children?.()}
 </div>
