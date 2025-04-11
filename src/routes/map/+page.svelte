602f345d123c4f89788ab9ba52b23b68531c6af7
Pandananana <andreaswendelboe.dk@gmail.com>
2025-04-11 09:24:06 +0200

    Map component added to map page and displays the full height of the available screen (excluding tabbar)

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import Map from "$lib/components/features/Map.svelte";
 	import type { PageData } from "./$types";
 
 	let { data }: { data: PageData } = $props();
 </script>
+<div class="h-full flex flex-cl">
+	<Map />
+</div>
