7e4b86f09b6d2680a793d505f91d9b2984889006
Pandananana <andreaswendelboe.dk@gmail.com>
2025-04-11 13:32:10 +0200

    Waypoint cards snap scroll added

--- src/lib/components/features/map/ChargerWaypoints.svelte
+++ src/lib/components/features/map/ChargerWaypoints.svelte
@@ -6,11 +6,13 @@
     export let onNavigate: (id: string) => void = () => {};
 </script>
 
-<div class="flex overflow-x-auto gap-5 py-5 px-5 w-full no-scrollbar">
-    {#each chargers as charger (charger.stationid)}
-        <WaypointCard 
-            {charger} 
-            onNavigate={() => onNavigate(charger.stationid)} 
-        />
+<div class="flex overflow-x-auto gap-5 py-5 px-0 w-full no-scrollbar scroll-smooth snap-x snap-mandatory">
+    {#each chargers as charger, index (charger.stationid)}
+        <div class="snap-center w-10/12 flex-shrink-0 {index === 0 ? 'ml-10' : ''}">
+            <WaypointCard 
+                {charger} 
+                onNavigate={() => onNavigate(charger.stationid)} 
+            />
+        </div>
     {/each}
 </div>
