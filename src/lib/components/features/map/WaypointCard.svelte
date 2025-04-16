5cd39b89a00d36fbf1beeccac18a8efa8db367dd
Pandananana <andreaswendelboe.dk@gmail.com>
2025-04-16 10:19:12 +0200

    Charger waypoint card design changed. Now shows city

--- src/lib/components/features/map/WaypointCard.svelte
+++ src/lib/components/features/map/WaypointCard.svelte
@@ -3,6 +3,10 @@
 	import type { ChargerStation } from "$lib/types/chargers";
 	import Glass from "$lib/components/ui/Glass.svelte";
 	import { selectedChargerID, calculateOpeningHours } from "$lib/services/map";
+	import Bolt from "~icons/mdi/lightning-bolt";
+	import MapMarker from "~icons/mdi/map-marker";
+	import Clock from "~icons/mdi/clock-time-four-outline";
+	import { slide, fly } from "svelte/transition";
 
 	let {
 		charger,
@@ -16,6 +20,7 @@
 
 	let isSelected: boolean = $state(false);
 	let openingHours: string = $state("");
+	let available: boolean = $derived(charger.connector === "Available");
 
 	$effect(() => {
 		isSelected = charger.stationid === $selectedChargerID;
@@ -29,51 +34,56 @@
 </script>
 
 <Glass>
-	<div class="p-4 flex flex-col gap-3">
-		<div class="flex items-center justify-between">
-			<div class="flex items-end text-lk-blue-50 font-bold">
-				<!-- TODO: Implement correct price based on discounts and other factors -->
-				<span class="text-3xl">{charger.prices.nominal}</span>
-				<span class="text-lg font-light ml-2">
-					<span class="font-bold">DKK</span>
-					/kWh
+	<div class="flex flex-col">
+		<div class="border-b border-lk-blue-800 p-2 text-lg">
+			{charger.location.city}
+		</div>
+
+		<div class="flex justify-around p-3">
+			<div class="flex flex-col items-center gap-1.5">
+				<div class="p-1.5 flex items-center bg-lk-blue-800/50 rounded-full">
+					<Bolt class="text-lg text-lk-green-400" />
+				</div>
+				<span class="text-lg font-medium text-lk-green-400">
+					{charger.prices.nominal.toFixed(2)} kr.
 				</span>
 			</div>
-			<div
-				class="bg-lk-green-400 font-bold rounded-md px-2 py-1 text-sm flex items-center backdrop-blur-sm
-					{charger.connector === 'Available' ? 'bg-lk-green-400 text-lk-green-900' : 'bg-lk-red-700 text-white'}"
-			>
-				<span>{distance.toFixed(1)} km</span>
+			<div class="flex flex-col items-center gap-1.5">
+				<div class="p-1.5 flex items-center bg-lk-blue-800/50 rounded-full">
+					<MapMarker class="text-lg {available ? "text-lk-blue-50":"text-lk-red-600"}" />
+				</div>
+				<span class="text-lg font-medium {available ? "text-lk-blue-50":"text-lk-red-600"}">
+					{distance.toFixed(2)} km
+				</span>
 			</div>
 		</div>
 
 		{#if isSelected}
-			<div class="flex flex-col items-start gap-1.5 mb-1">
-				<span class="text-xs font-light text-lk-blue-200">Opening Hours:</span>
-				<div class="p-2 w-full rounded-2xl text-lg font-medium border border-lk-blue-800">
+			<div class="flex flex-col p-4 gap-2 border-t border-lk-blue-800 bg-lk-blue-950" transition:slide>
+				<div class="flex gap-2 text-lg text-lk-blue-100/80 mb-2">
+					<Clock  />
 					{openingHours}
 				</div>
-			</div>
-
-			<div class="flex justify-between gap-3">
-				<button
-					onclick={onNavigate}
-					disabled={charger.connector !== "Available"}
-					class="flex-1 backdrop-blur-sm transition-all p-1.5 rounded-2xl text-lg font-medium shadow-sm
+				<div class="flex justify-between gap-3">
+					<button
+						onclick={onNavigate}
+						disabled={charger.connector !== "Available"}
+						class="flex-1 backdrop-blur-sm transition-all p-1.5 rounded-2xl text-lg font-medium shadow-sm
 						{charger.connector === 'Available' ? 'bg-lk-blue-200 text-lk-blue-900' : 'bg-lk-blue-900 text-gray-300'}"
-				>
-					{#if charger.connector === "Available"}
-						Reserve
-					{:else}
-						Charging...
-					{/if}
-				</button>
-				<button
-					onclick={onNavigate}
-					class="bg-lk-blue-200 backdrop-blur-sm text-gray-800 transition-all p-2 rounded-2xl font-medium shadow-sm"
-				>
-					<Directions class="text-xl" />
-				</button>
+					>
+						{#if available}
+							Reserve
+						{:else}
+							Charging...
+						{/if}
+					</button>
+					<button
+						onclick={onNavigate}
+						class="bg-lk-blue-200 backdrop-blur-sm text-gray-800 transition-all p-2 rounded-2xl font-medium shadow-sm"
+					>
+						<Directions class="text-xl" />
+					</button>
+				</div>
 			</div>
 		{/if}
 	</div>
