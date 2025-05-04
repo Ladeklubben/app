bdb671ac45598a98ab172da9d045c546e3460684
Pandananana <andreaswendelboe.dk@gmail.com>
2025-05-04 11:10:31 +0200

    Add dynamic gap property to Form component for flexible spacing

--- src/lib/components/ui/Form.svelte
+++ src/lib/components/ui/Form.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	let { id = "form", children, handleSubmit } = $props();
+	let { id = "form", children, handleSubmit, gap=5 } = $props();
 </script>
 
-<form onsubmit={handleSubmit} {id} novalidate class="flex flex-col gap-5 w-full">
+<form onsubmit={handleSubmit} {id} novalidate class="flex flex-col gap-{gap} w-full">
 	{@render children?.()}
 </form>
