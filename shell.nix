04ff7c7646382f3b16e152269cd9a493a3bef03d
Pandananana <andreaswendelboe.dk@gmail.com>
2025-06-23 14:21:11 +0200

    Removed unnecessary android libs, since we have a Github actions that builds for android anyways

--- shell.nix
+++ shell.nix
@@ -3,15 +3,5 @@ with import <nixpkgs> {};
 pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs_22
-    jdk21
-    android-tools
-    android-studio
   ];
-
-  shellHook = ''
-    export CAPACITOR_ANDROID_STUDIO_PATH=/run/current-system/sw/bin/android-studio
-    export ANDROID_SDK_ROOT=/home/aw/Android/Sdk
-    export JAVA_HOME=${pkgs.jdk21}/lib/openjdk
-    export PATH=$JAVA_HOME/bin:$PATH
-  '';
 }
