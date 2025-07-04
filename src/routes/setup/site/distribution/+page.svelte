433c485748663f000026477e29564404b3a71dd2
Pandananana <andreaswendelboe.dk@gmail.com>
2025-07-04 13:10:00 +0200

    Replace InputField with DropDownInput for improved selection handling in distribution form

--- src/routes/setup/site/distribution/+page.svelte
+++ src/routes/setup/site/distribution/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import Subpage from "$lib/components/ui/Subpage.svelte";
-	import InputField from "$lib/components/ui/InputField.svelte";
 	import Form from "$lib/components/ui/Form.svelte";
 	import { fetchPowerCompanies, siteFormData, powerCompanies, createNewSite } from "$lib/services/site";
 	import BottomButton from "$lib/components/ui/BottomButton.svelte";
 	import { onMount } from "svelte";
 	import { goto } from "$app/navigation";
+	import DropDownInput from "$lib/components/ui/inputs/DropDownInput.svelte";
 
 	let loading = false;
 
@@ -29,15 +29,7 @@
 				To calculate your live electricity prices, and therefore charge your car in a price-efficient manner,
 				please select the power distribution company you use.
 			</p>
-
-			<InputField
-				id="company"
-				type="dropdown"
-				label="Distribution Company"
-				bind:value={$siteFormData.distributionCompany}
-				options={$powerCompanies}
-				error=""
-			/>
+			<DropDownInput id="company" label="Distribution Company" bind:value={$siteFormData.distributionCompany} options={$powerCompanies} />
 		</Form>
 	</div>
 	<BottomButton activeDot={4} totalDots={4} formID="form" buttonText="Done" {loading} />
